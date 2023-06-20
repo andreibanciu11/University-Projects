f03c592536b6f5de6ff20adfc2f43ac515ee14bb
Andrei Banciu <127332164+andreibanciu11@users.noreply.github.com>
2023-06-20 19:32:50 +0300

    Add files via upload

---
+++ Year II/Second Semester/Database Management Systems/Practic/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Practic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvCompanies = new System.Windows.Forms.DataGridView();
+            this.dgvInternships = new System.Windows.Forms.DataGridView();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.removeButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompanies)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInternships)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvCompanies
+            // 
+            this.dgvCompanies.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCompanies.Location = new System.Drawing.Point(86, 61);
+            this.dgvCompanies.Name = "dgvCompanies";
+            this.dgvCompanies.RowHeadersWidth = 51;
+            this.dgvCompanies.RowTemplate.Height = 24;
+            this.dgvCompanies.Size = new System.Drawing.Size(596, 354);
+            this.dgvCompanies.TabIndex = 0;
+            // 
+            // dgvInternships
+            // 
+            this.dgvInternships.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvInternships.Location = new System.Drawing.Point(802, 61);
+            this.dgvInternships.Name = "dgvInternships";
+            this.dgvInternships.RowHeadersWidth = 51;
+            this.dgvInternships.RowTemplate.Height = 24;
+            this.dgvInternships.Size = new System.Drawing.Size(596, 354);
+            this.dgvInternships.TabIndex = 1;
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(679, 453);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(103, 52);
+            this.saveButton.TabIndex = 2;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(1050, 453);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(103, 52);
+            this.removeButton.TabIndex = 3;
+            this.removeButton.Text = "Remove";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1467, 581);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.dgvInternships);
+            this.Controls.Add(this.dgvCompanies);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCompanies)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvInternships)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvCompanies;
+        private System.Windows.Forms.DataGridView dgvInternships;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button removeButton;
+    }
+}
+
---
+++ Year II/Second Semester/Database Management Systems/Practic/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practic
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=DESKTOP-NN3D6V8\\SQLEXPRESS;Initial Catalog=InternshipDB;Integrated Security=True";
+
+        private SqlDataAdapter daParent, daChild;
+        private BindingSource bsParent, bsChild;
+        private DataSet dataSet;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string sqlConn = connectionString;
+            dataSet = new DataSet();
+
+            daParent = new SqlDataAdapter("SELECT * FROM companies", sqlConn);
+            SqlCommandBuilder cb = new SqlCommandBuilder(daParent);
+            daParent.Fill(dataSet, "companies");
+
+            daChild = new SqlDataAdapter("SELECT * FROM internships", sqlConn);
+            cb = new SqlCommandBuilder(daChild);
+            daChild.Fill(dataSet, "internships");
+
+            DataRelation drel = new DataRelation("fk_child_parent",
+                dataSet.Tables["companies"].Columns["id"],
+                dataSet.Tables["internships"].Columns["companyID"]);
+            dataSet.Relations.Add(drel);
+
+            bsParent = new BindingSource();
+            bsParent.DataSource = dataSet;
+            bsParent.DataMember = "companies";
+
+            bsChild = new BindingSource();
+            bsChild.DataSource = bsParent;
+            bsChild.DataMember = "fk_child_parent";
+
+            dgvCompanies.DataSource = bsParent;
+            dgvInternships.DataSource = bsChild;
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (dgvInternships.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dgvInternships.SelectedRows[0];
+                DataRowView selectedRowView = (DataRowView)selectedRow.DataBoundItem;
+
+                int primaryKeyValue = Convert.ToInt32(selectedRowView.Row["id"]);
+
+                selectedRowView.Delete();
+                this.daParent.Update(this.dataSet, "companies");
+                this.daChild.Update(this.dataSet, "internships");
+            }
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            this.daParent.Update(this.dataSet, "companies");
+            this.daChild.Update(this.dataSet, "internships");
+        }
+    }
+}
