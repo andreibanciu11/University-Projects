784d9a45826640d2a1259ee61082b97aecbcfb42
Andrei Banciu <127332164+andreibanciu11@users.noreply.github.com>
2023-06-20 19:34:41 +0300

    Add files via upload

---
+++ Year II/Second Semester/Database Management Systems/Lab1_Assignment/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿using System;
+
+namespace Lab1_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 66);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(539, 334);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.SelectionChanged += new System.EventHandler(this.dataGridView1_SelectionChanged);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(853, 66);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(569, 334);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(735, 66);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 37);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(735, 109);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(735, 152);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 37);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Remove";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1445, 580);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Year II/Second Semester/Database Management Systems/Lab1_Assignment/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+
+namespace Lab1_Assignment
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "Data Source=DESKTOP-NN3D6V8\\SQLEXPRESS;Initial Catalog=RapDB;Integrated Security=True";
+        private SqlDataAdapter daParent, daChild;
+        private BindingSource bsParent, bsChild;
+        private DataSet dataSet;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string sqlConn = connectionString;
+            dataSet = new DataSet();
+
+            daParent = new SqlDataAdapter(getParentQuery(), sqlConn);
+            SqlCommandBuilder cb = new SqlCommandBuilder(daParent);
+            daParent.Fill(dataSet, getParentTable());
+
+            daChild = new SqlDataAdapter(getChildQuery(), sqlConn);
+            cb = new SqlCommandBuilder(daChild);
+            daChild.Fill(dataSet, getChildTable());
+
+            DataRelation drel = new DataRelation("fk_child_parent",
+                dataSet.Tables[getParentTable()].Columns[getPKName()],
+                dataSet.Tables[getChildTable()].Columns[getFKName()]);
+            dataSet.Relations.Add(drel);
+
+            bsParent = new BindingSource();
+            bsParent.DataSource = dataSet;
+            bsParent.DataMember = getParentTable();
+
+            bsChild = new BindingSource();
+            bsChild.DataSource = bsParent;
+            bsChild.DataMember = "fk_child_parent";
+
+            dataGridView1.DataSource = bsParent;
+            dataGridView2.DataSource = bsChild;
+        }
+
+        private void dataGridView1_SelectionChanged(object sender, EventArgs e)
+        {
+            int parentID = (int)dataGridView1.CurrentRow.Cells[getPKName()].Value;
+
+            BindingSource childBindingSource = (BindingSource)dataGridView2.DataSource;
+            if (childBindingSource != null)
+            {
+                childBindingSource.Filter = $"{getFKName()} = {parentID}";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) // Add button
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e) // Update button
+        {
+            this.daParent.Update(this.dataSet, getParentTable());
+            this.daChild.Update(this.dataSet, getChildTable());
+        }
+
+        private void button3_Click(object sender, EventArgs e) // Delete Button
+        {
+            if (dataGridView2.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView2.SelectedRows[0];
+                DataRowView selectedRowView = (DataRowView)selectedRow.DataBoundItem;
+
+                int primaryKeyValue = Convert.ToInt32(selectedRowView.Row[getPKNameChild()]);
+
+                dataGridView2.Refresh();
+
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    SqlCommand command = new SqlCommand();
+                    command.Connection = connection;
+                    command.CommandText = "DELETE FROM " + getChildTable() + " WHERE " + getPKNameChild() + " = @PrimaryKey";
+                    command.Parameters.AddWithValue("@PrimaryKey", primaryKeyValue);
+
+                    command.ExecuteNonQuery();
+                }
+
+                selectedRowView.Delete();
+            }
+        }
+
+        private string getPKName()
+        {
+            return ConfigurationManager.AppSettings["parent_table_pk"];
+        }
+
+        private string getFKName()
+        {
+            return ConfigurationManager.AppSettings["child_table_fk"];
+        }
+
+        private string getPKNameChild()
+        {
+            return ConfigurationManager.AppSettings["child_table_pk"];
+        }
+
+        private string getParentTable()
+        {
+            return ConfigurationManager.AppSettings["parent_table"];
+        }
+
+        private string getParentQuery()
+        {
+            return ConfigurationManager.AppSettings["parent_query"];
+        }
+
+        private string getChildTable()
+        {
+            return ConfigurationManager.AppSettings["child_table"];
+        }
+        private string getChildQuery()
+        {
+            return ConfigurationManager.AppSettings["child_query"];
+        }
+
+        private string getConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["connection_string"].ConnectionString.ToString();
+        }
+    }
+}
