20c34d36fc9c33bab3c6f78dcdcd8dc119b2294c
Andrei Banciu <127332164+andreibanciu11@users.noreply.github.com>
2023-10-16 12:07:42 +0300

    Delete Year III/First Semester directory

--- Year III/First Semester/FLCD/Lab/Lab3/SymbolTable/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        SymbolTable symbolTable = new SymbolTable(100);
-        symbolTable.put("andrei");
-        symbolTable.put("banciu");
-        symbolTable.put("banciu");
-        symbolTable.printAll();
-        }
-    }
